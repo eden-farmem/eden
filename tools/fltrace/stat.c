9992c878c83e3ec1893a7cd7aa67bd6e1d70609f
Anil Yelam <ayelam@vmware.com>
2023-01-15 01:23:07 -0800

    fltrace: gather virt memory stats from OS

--- tools/fltrace/stat.c
+++ tools/fltrace/stat.c
@@ -17,12 +17,46 @@
 #include <runtime/timer.h>
 
 #define STAT_INTERVAL_SECS  1
-#define MAX_STAT_STR_LEN    1500
+#define MAX_STAT_STR_LEN    2048
 
 static inline int append_stat(char *pos, size_t len, 
     const char *name, uint64_t val)
 {
     return snprintf(pos, len, "%s:%ld,", name, val);
+}
+
+/* get value of the process virt mem counter from /proc/self/status file */
+unsigned long long get_process_vm_counter(const char* name)
+{
+    const int line_size = 512;
+    char line[line_size];
+    unsigned long long value;
+    FILE *file;
+    bool found;
+    
+    file = fopen("/proc/self/status", "r");
+    if (!file)
+        return 0;
+
+    found = false;
+    while (fgets(line, line_size, file) != NULL) {
+        int i = strlen(line);
+        assert(i > 6);
+        if (strncmp(line, name, strlen(name)) == 0) {
+            /* assumes that a digit will be found and the line ends in " Kb" */
+            const char *p = line;
+            while (*p < '0' || *p > '9') p++;
+            line[i - 3] = '\0';
+            value = atoi(p);
+            found = true;
+            break;
+        }
+    }
+    fclose(file);
+    BUG_ON(!found);
+
+    value *= 1024;  /* to bytes */
+    return value;
 }
 
 /* gather all rmem stats and write to the buffer */
@@ -46,48 +80,40 @@ static inline int rstat_write_buf(char *buf, size_t len)
         }
     }
 
+#define APPEND_STAT(name,val)   \
+    ret = append_stat(pos, end - pos, name, val); \
+    if (ret < 0) { \
+        return -EINVAL; \
+    } else if (ret >= end - pos) { \
+        return -E2BIG; \
+    } \
+    pos += ret;
+
     /* write out all thr stats to the buffer */
     pos = buf;
     end = buf + len;
     for (j = 0; j < RSTAT_NR; j++) {
-        ret = append_stat(pos, end - pos, rstat_names[j], rstats[j]);
-        if (ret < 0) {
-            return -EINVAL;
-        } else if (ret >= end - pos) {
-            return -E2BIG;
-        }
-        pos += ret;
+        APPEND_STAT(rstat_names[j], rstats[j]);
     }
 
-    /* report memory used */
-    ret = append_stat(pos, end - pos,
-        "memory_used", atomic64_read(&memory_used));
-    if (ret < 0) {
-        return -EINVAL;
-    } else if (ret >= end - pos) {
-        return -E2BIG;
-    }
-    pos += ret;
+    /* report local memory usage stats */
+    APPEND_STAT("memory_used", atomic64_read(&memory_used));
+    APPEND_STAT("memory_allocd", atomic64_read(&memory_allocd));
+    APPEND_STAT("memory_freed", atomic64_read(&memory_freed));
 
-    /* report memory allocd */
-    ret = append_stat(pos, end - pos,
-        "memory_allocd", atomic64_read(&memory_allocd));
-    if (ret < 0) {
-        return -EINVAL;
-    } else if (ret >= end - pos) {
-        return -E2BIG;
-    }
-    pos += ret;
-
-    /* report memory freed */
-    ret = append_stat(pos, end - pos,
-        "memory_freed", atomic64_read(&memory_freed));
-    if (ret < 0) {
-        return -EINVAL;
-    } else if (ret >= end - pos) {
-        return -E2BIG;
-    }
-    pos += ret;
+    /* report process vm stats from the status file */
+    APPEND_STAT("vm_peak", get_process_vm_counter("VmPeak"));
+    APPEND_STAT("vm_size", get_process_vm_counter("VmSize"));
+    APPEND_STAT("vm_lock", get_process_vm_counter("VmLck"));
+    APPEND_STAT("vm_pin", get_process_vm_counter("VmPin"));
+    APPEND_STAT("vm_hwm", get_process_vm_counter("VmHWM"));
+    APPEND_STAT("vm_rss", get_process_vm_counter("VmRSS"));
+    APPEND_STAT("vm_data", get_process_vm_counter("VmData"));
+    APPEND_STAT("vm_stk", get_process_vm_counter("VmStk"));
+    APPEND_STAT("vm_exe", get_process_vm_counter("VmExe"));
+    APPEND_STAT("vm_lib", get_process_vm_counter("VmLib"));
+    APPEND_STAT("vm_pte", get_process_vm_counter("VmPTE"));
+    APPEND_STAT("vm_swap", get_process_vm_counter("VmSwap"));
 
     pos[-1] = '\0'; /* clip off last ',' */
     return 0;
