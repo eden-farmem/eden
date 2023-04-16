3f3dc68bb06ffb6c167dbeb7e241b51d22acc919
Anil Yelam <ayelam@vmware.com>
2023-04-15 21:05:55 -0700

    rmem: wrapper hints for hinting multiple pages and sequential page access

--- inc/runtime/pgfault.h
+++ inc/runtime/pgfault.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "base/stddef.h"
+
 /* runtime internal */
 int __vdso_init();
 bool __is_fault_pending(void* address, bool write, bool hint_eviction);
@@ -50,69 +52,66 @@ void thread_park_on_fault(void* address, bool write, int rdahead, int evprio);
 #define EDEN_MAX_READAHEAD      63
 #define PAGE_ID(addr) (((unsigned long) addr) & EDEN_PAGE_ID_MASK)
 
-/* estimate number of pages to read-ahead based on a range */
-#define readahead(addr, size)     \
-    ((((unsigned long) addr + size - 1) >> EDEN_PAGE_SHIFT) -  \
-    ((unsigned long) addr >> EDEN_PAGE_SHIFT))
 
-/* hints that account for sequential access pattern */
-#define hint_seq_read_fault(addr)		                            \
+/**
+ * Hints that account for sequential page access to skip
+ * unnecessary page checks 
+ **/
+
+#if defined(REMOTE_MEMORY_HINTS) || defined(EDEN_HINTS)
+#define hint_fault_seq(addr, write, rdahead, prio)		            \
 ({                                                                  \
     static unsigned long __last_page = 0;                           \
     if (unlikely(!__last_page || PAGE_ID(addr) != __last_page)) {   \
         __last_page = PAGE_ID(addr);                                \
-        hint_read_fault(addr);	                                    \
+        hint_fault(addr, write, rdahead, prio);	                    \
     }                                                               \
 })
 
-#define hint_seq_read_fault_rdahead(addr, rdahead)		            \
+#define hint_fault_seq_rdahead(addr, write, rdahead, prio)		    \
 ({                                                                  \
     static unsigned long __start = 0;	                            \
     static unsigned long __end = 0;		                            \
-    if (unlikely(!__start || addr < __start ||  addr >= __end)) {   \
+    if (unlikely(!__start || (unsigned long) addr < __start ||      \
+            (unsigned long) addr >= __end)) {                       \
         __start = PAGE_ID(addr);                                    \
-        __end = __start + (rdahead << EDEN_PAGE_SHIFT);		        \
-        hint_read_fault_rdahead(addr, rdahead);	                    \
+        __end = __start + ((1 + rdahead) << EDEN_PAGE_SHIFT);	    \
+        hint_fault(addr, write, rdahead, prio);	                    \
     }                                                               \
 })
+#else
+#define hint_fault_seq(addr)                        do {} while(0)
+#define hint_fault_seq_rdahead(addr, rdahead)       do {} while(0)
+#endif
 
-#define hint_seq_write_fault(addr)		                            \
-({                                                                  \
-    static unsigned long __last_page = 0;                           \
-    if (unlikely(!__last_page || PAGE_ID(addr) != __last_page)) {   \
-        __last_page = PAGE_ID(addr);                                \
-        hint_write_fault(addr);	                                    \
-    }                                                               \
-})
-
-#define hint_seq_write_fault_rdahead(addr, rdahead)		            \
-({                                                                  \
-    static unsigned long __start = 0;	                            \
-    static unsigned long __end = 0;		                            \
-    if (unlikely(!__start || addr < __start ||  addr >= __end)) {   \
-        __start = PAGE_ID(addr);                                    \
-        __end = __start + (rdahead << EDEN_PAGE_SHIFT);		        \
-        hint_write_fault_rdahead(addr, rdahead);	                \
-    }                                                               \
-})
+#define hint_seq_read_fault(addr)   hint_fault_seq(addr, false, 0, 0)
+#define hint_seq_write_fault(addr)  hint_fault_seq(addr, true,  0, 0)
+#define hint_seq_read_fault_rdahead(addr, rdahead)  hint_fault_seq_rdahead(addr, false, rdahead, 0)
+#define hint_seq_write_fault_rdahead(addr, rdahead) hint_fault_seq_rdahead(addr, true,  rdahead, 0)
 
 /** 
- * Page boundary-safe hints for small items
+ * Region-based hints to get a multi-page region at once
  */
-#define hint_read_fault_pb_safe(start, size)                            \
-    if (unlikely(PAGE_ID((start)) !=                                    \
-            PAGE_ID((unsigned long) (start) + (size) - 1))) {           \
-        hint_read_fault_rdahead(start, 1);                              \
-        hint_read_fault((void*)((unsigned long) (start) + (size) - 1)); \
-    } else {                                                            \
-        hint_read_fault(start);                                         \
-    }
 
-#define hint_write_fault_pb_safe(start, size)                           \
-    if (unlikely(PAGE_ID((start)) !=                                    \
-            PAGE_ID((unsigned long) (start) + (size) - 1))) {           \
-        hint_write_fault_rdahead(start, 1);                             \
-        hint_write_fault((void*)((unsigned long) (start) + (size) - 1));\
-    } else {                                                            \
-        hint_write_fault(start);                                        \
-    }
+#define hint_fault_region(addr, size, write, max_rdahead, prio)                \
+({                                                                             \
+    int rdahead;                                                               \
+    char *__start, *__end;                                                     \
+                                                                               \
+    __start = (char*)(align_down((unsigned long) addr, EDEN_PAGE_SIZE));       \
+    __end = (char*)(align_down((unsigned long) addr + size, EDEN_PAGE_SIZE));  \
+    while (__start <= __end) {                                                 \
+        rdahead = MIN((__end - __start) >> EDEN_PAGE_SHIFT, max_rdahead);      \
+        hint_fault(__start, write, rdahead, prio)                              \
+        __start += (((1 + rdahead)) << EDEN_PAGE_SHIFT);                       \
+    }                                                                          \
+})
+
+#define hint_read_fault_region(addr, size)                      hint_fault_region(addr, size, false, 0, 0)
+#define hint_write_fault_region(addr, size)                     hint_fault_region(addr, size, true,  0, 0)
+#define hint_read_fault_region_rdahead(addr, size, rdahead)     hint_fault_region(addr, size, false, rdahead, 0)
+#define hint_write_fault_region_rdahead(addr, size, rdahead)    hint_fault_region(addr, size, true,  rdahead, 0)
+
+/* back-compat API */
+#define hint_read_fault_pbsafe  hint_read_fault_region
+#define hint_write_fault_pbsafe hint_write_fault_region
